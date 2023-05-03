05d6f839e3424c36ed0da9920ebf2d9d79dd863d
Roman Vitolo <68485569+RomanVitolo@users.noreply.github.com>
2023-05-02 23:39:02 -0300

    Next Commit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/RomanVitolo/GoCourse/variables"
 )
@@ -10,4 +11,19 @@ func main() {
 	state, text := variables.ConvertToText(1588)
 	fmt.Println(state)
 	fmt.Println(text)
+	os := runtime.GOOS
+	if os == "linux" || os == "OS X." {
+		fmt.Println("This is not Windows", os)
+	} else {
+		fmt.Println("This is Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("This is Linux")
+	case "darwin":
+		fmt.Println("This is Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
